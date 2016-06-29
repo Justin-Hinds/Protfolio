2398d37f2c8f75a3814e84635936f09104a1ede7
Justin-Hinds <justinhinds@gmail.com>
2016-06-29 12:52:20 -0700

    start destiny

---
+++ README.txt
@@ -0,0 +1,5 @@
+Developer: Justin Hinds
+Recipe Builder Version 0.2.5
+Git - https://github.com/Justin-Hinds/Protfolio/commits/master
+
+Currently the only thing working is the Facebook sign in. Everything else is a skeleton in place for development next week. Cocoa pods was used so yo may need to use the .xcworkspace file in order to get it to build properly.
--- Recipe Builder/Recipe Builder/ViewController.swift
+++ Recipe Builder/Recipe Builder/ViewController.swift
@@ -8,12 +8,31 @@
 
 import UIKit
 
+let recipeSearch: String = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search?limitLicense=false&number=10&offset=0&query=pasta"
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = NSURL(string: recipeSearch)!
+        let session = NSURLSession.sharedSession()
+        let request = NSURLRequest(URL: url)
+        let connection = NSURLConnection(request: request, delegate: self)!
+        let task = session.dataTaskWithRequest(request){ (data, response, error) in
+            guard let realResponse = response as? NSHTTPURLResponse where
+                realResponse.statusCode == 200 else {
+                    print("Not a 200 response ")
+                    return
+            }
+            do{
+                print(response)
+            }catch{
+                print("bad stuff")
+            }
+        }
+            task.resume()
+        // Do any additional s().self.self.selfetup after loading the view, typically from a nib.
         //FACEBOOK
         if (FBSDKAccessToken.currentAccessToken() != nil)
         {
@@ -25,7 +44,7 @@ class ViewController: UIViewController {
             self.view!.addSubview(loginView)
             loginView.center = self.view!.center
             loginView.readPermissions = ["public_profile", "email", "user_friends"]
-            //loginView.delegate = self
+            //loginView.delegate = sel
         }
 
     }
